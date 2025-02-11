3bed496129e30cfbc1a179453b0ff7f63fb827d9
gombasan <xogmlwkdi@naver.com>
2025-02-11 21:21:59 +0900

    팀즈 설정 롤백

--- src/middleware.js
+++ src/middleware.js
@@ -12,15 +12,15 @@ export function middleware(request) {
     },
   });
 
-  // // Content-Security-Policy 및 X-Frame-Options 헤더 추가
-  // response.headers.set(
-  //   "Content-Security-Policy",
-  //   "frame-ancestors 'self' https://*.teams.microsoft.com;"
-  // );
-  // response.headers.set(
-  //   "X-Frame-Options",
-  //   "ALLOW-FROM https://teams.microsoft.com"
-  // );
+  // Content-Security-Policy 및 X-Frame-Options 헤더 추가
+  response.headers.set(
+    "Content-Security-Policy",
+    "frame-ancestors 'self' https://*.teams.microsoft.com;"
+  );
+  response.headers.set(
+    "X-Frame-Options",
+    "ALLOW-FROM https://teams.microsoft.com"
+  );
 
   return response;
 }
